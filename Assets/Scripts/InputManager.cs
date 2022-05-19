093bd4a31963b51fb3bd4125016b73290d5ed9ed
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2022-05-19 23:02:53 +0200

    start to create map

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static TileManager;
 
 public class InputManager : MonoBehaviour
 {
@@ -14,6 +16,8 @@ public class InputManager : MonoBehaviour
         EditMap
     }
     public Transform rayAnchor;
+
+    public TileTypes SelectedTile;
 
     private bool debouncing = false;
     private float lastHit = 0;
@@ -45,26 +49,67 @@ public class InputManager : MonoBehaviour
     bool movingMap = false;
     float delay = 1.0f;
     float startDelay = 0.0f;
+    GameObject selectedTilePrefab;
 
     void Update()
     {
         var buttonOne = OVRInput.Get(OVRInput.Button.One);
         var buttonTwo = OVRInput.Get(OVRInput.Button.Two);
 
-        Log.text = "GameState? " + GameState;
-        /*
-        if (GameState == GameStates.NewMap) {
+        if (GameState == GameStates.EditMap) {
             if(OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger, OVRInput.Controller.Touch) > 0.5f) {
                 GameState = GameStates.InsertTile;
                 MenuManager.InsertTileMode();
             }
         }
-        */
 
         if(GameState == GameStates.InsertTile) {
             if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger, OVRInput.Controller.Touch) < 0.1f) {
+                Destroy(selectedTilePrefab);
                 GameState = GameStates.NewMap;
                 MenuManager.EditTileMode();
+            }
+            RaycastHit hit;
+            if (buttonOne) {
+                if (Physics.Raycast(rayAnchor.position, rayAnchor.forward, out hit)) {
+                    var collider = hit.transform.gameObject.GetComponent<TileCollider>();
+                    if (collider != null) {
+                        SelectedTile = collider.TileManager.GetComponent<TileManager>().TileType;
+                        Destroy(selectedTilePrefab);
+                        var group = MenuManager.Tiles.FirstOrDefault(x => x.Tiles.Select(tile => tile.TileType).Contains(SelectedTile));
+                        if (group == null) {
+                            return;
+                        }
+                        var prefab = group.Tiles.FirstOrDefault(x => x.TileType == SelectedTile).Prefab;
+                        selectedTilePrefab = Instantiate(prefab);
+                        selectedTilePrefab.transform.parent = GameObject.FindGameObjectWithTag("MapRoot").transform;
+                    }
+                    var referencePlane = this.transform.gameObject.GetComponent<ReferencePlaneCollider>();
+                    if(referencePlane != null && !debouncing) {
+                        var go = Instantiate(selectedTilePrefab);
+                        Vector3 point = hit.point;
+                        go.transform.position = new Vector3(Mathf.Round(point.x * 20) / 20, Mathf.Round(point.y * 20) / 20, Mathf.Round(point.z * 20) / 20);
+                    }
+                }
+            }
+            
+            if (Physics.Raycast(rayAnchor.position, rayAnchor.forward, out hit)) {
+                var collider = hit.transform.gameObject.GetComponent<ReferencePlaneCollider>();
+                if (collider != null) {
+                    Vector3 point = hit.point;
+                    selectedTilePrefab.transform.position = new Vector3(Mathf.Round(point.x * 20) / 20, Mathf.Round(point.y * 20) / 20, Mathf.Round(point.z * 20) / 20);
+                }
+            }
+
+            if (buttonTwo && !debouncing) {
+                if (Physics.Raycast(rayAnchor.position, rayAnchor.forward, out hit)) {
+                    var collider = hit.transform.gameObject.GetComponent<TileCollider>();
+                    if (collider != null) {
+                        collider.Rotate();
+                    }
+                }
+                debouncing = true;
+                lastHit = Time.realtimeSinceStartup;
             }
         }
 
@@ -112,12 +157,17 @@ public class InputManager : MonoBehaviour
                 var mapRoot = GameObject.FindGameObjectWithTag("MapRoot").GetComponent<MapManager>();
                 mapRoot.MoveWithDelta(delta);
             }
+        } else {
+            movingMap = false;
+            rotatingRootClockwise = false;
+            rotatingRootCounterClockwise = false;
+            rigthHandStartingPos = rayAnchor.position;
         }
 
         if (Time.realtimeSinceStartup - lastHit > debounce) {
             debouncing = false;
         }
-
+        /*
         var primaryHandTrigger = OVRInput.Get(OVRInput.Button.Two);
 
         if(!primaryHandTrigger && movingRoot) {
@@ -143,5 +193,6 @@ public class InputManager : MonoBehaviour
                 }
             }
         }
+        */
     }
 }
