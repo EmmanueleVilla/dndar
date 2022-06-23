d2d624653f9be467c4e3e828d20c43786d6f29bf
Emmanuele Villa <emmanuele.villa@finconsgroup.com>
2022-06-23 17:55:31 +0200

    added tiles quantization

---
+++ Assets/Scripts/Quantization/SewerCornerQuantization.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using DndCore.Map;
+using UnityEngine;
+
+public class SewerCornerQuantization : TileQuantization
+{
+    public override List<CellInfo> ToMap()
+    {
+        var cells = new List<CellInfo>();
+        var rotation = transform.localEulerAngles.y;
+        rotation = Mathf.Round(rotation / 90) * 90;
+        if (rotation == 0)
+        {
+            cells.Add(new CellInfo('G', 0, null, 0, 0));
+            cells.Add(new CellInfo(' ', 0, null, 1, 0));
+            cells.Add(new CellInfo(' ', 0, null, 0, 1));
+            cells.Add(new CellInfo(' ', 0, null, 1, 1));
+        }
+        if (rotation == 90)
+        {
+            cells.Add(new CellInfo(' ', 0, null, 0, 0));
+            cells.Add(new CellInfo(' ', 0, null, 1, 0));
+            cells.Add(new CellInfo('G', 0, null, 0, 1));
+            cells.Add(new CellInfo(' ', 0, null, 1, 1));
+        }
+        if (rotation == 180)
+        {
+            cells.Add(new CellInfo(' ', 0, null, 0, 0));
+            cells.Add(new CellInfo(' ', 0, null, 1, 0));
+            cells.Add(new CellInfo(' ', 0, null, 0, 1));
+            cells.Add(new CellInfo('G', 0, null, 1, 1));
+        }
+        if (rotation == 270)
+        {
+            cells.Add(new CellInfo(' ', 0, null, 0, 0));
+            cells.Add(new CellInfo('G', 0, null, 1, 0));
+            cells.Add(new CellInfo(' ', 0, null, 0, 1));
+            cells.Add(new CellInfo(' ', 0, null, 1, 1));
+        }
+        return cells;
+    }
+}
